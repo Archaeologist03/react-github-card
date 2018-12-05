0282aba61b45c38c9b64d2e5504db5f52a1f3576
Ilija <omniiknight@hotmail.com>
2018-12-06 00:44:42 +0100

    Updates

---
+++ precache-manifest.29e942deb031eb19b06dfb9b1f9c9d71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a7192d623ea34590ffe",
+    "url": "/react-github-card/static/js/runtime~main.0a7192d6.js"
+  },
+  {
+    "revision": "c2d4c983082e28e55ac6",
+    "url": "/react-github-card/static/js/main.c2d4c983.chunk.js"
+  },
+  {
+    "revision": "f59dae8204a44390f2e0",
+    "url": "/react-github-card/static/js/1.f59dae82.chunk.js"
+  },
+  {
+    "revision": "c2d4c983082e28e55ac6",
+    "url": "/react-github-card/static/css/main.289b4c64.chunk.css"
+  },
+  {
+    "revision": "e449b54a93e9e9183e26c42ecbdafd3b",
+    "url": "/react-github-card/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.bd8d85f39be1ae7cea2e696337983e5d.js"
+  "/react-github-card/precache-manifest.29e942deb031eb19b06dfb9b1f9c9d71.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/react-github-card/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
